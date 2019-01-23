3a5b9b68a59c74b3fb99c9f1e008ab2515eff8b1
Cam Couch <cameronmcouch@gmail.com>
2019-01-23 18:58:17 -0500

    fixed it

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,30 +22,58 @@ function getDamage () {
 }
 function startCombat() {
     let namePrompt = prompt("What is your name?");
-    if (namePrompt === namePrompt) {
-            let attackOrQuit = prompt("Would you like to attack or quit?");
-            if (attackOrQuit === "attack") {
-            while (attackOrQuit === "attack") {
-            if (wincondition === 0)  {
-            console.log(`Grant is donezo, ${namePrompt} is victorious!`)
-            break;
-            }
-            else if (userHP <= 0) {
-            console.log(`Grant proved to be too mighty, ${namePrompt} has lost the game.`)
-            break;
-            }
-            else if (bossHP <= 0 && userHP != 0 && wincondition != 0) {
-            bossHP = 10;
-            wincondition--;
-            console.log(`With a mighty blow from ${namePrompt}, Grant falls to the ground`);
-            }  
-            else {
-            userHP -= getDamage();
-            bossHP -= getDamage();
-            console.log(`${namePrompt}'s health is ${userHP}.`);
-            console.log(`Grant's health is ${bossHP}.`);
-            }
-        } //while loop
-    } else {console.log("Game Over");}
-    }//master if
-} //startcombat function
+    while (namePrompt === namePrompt) {
+        let attackOrQuit = prompt("Would you like to attack or quit?");
+        if (attackOrQuit === "attack") {
+          if (wincondition === 0) {
+          console.log(`Grant is donezo, ${namePrompt} is victorious!`)
+          break;
+        }
+          else if (userHP <= 0) {
+          console.log(`Grant proved to be too mighty, ${namePrompt} has lost the game.`)
+          break;
+        }
+          else if (bossHP <= 0 && userHP != 0 && wincondition != 0) {
+          bossHP = 10;
+          wincondition--;
+          console.log(`With a mighty blow from ${namePrompt}, Grant falls to the ground`);
+        }
+          else {
+          userHP -= getDamage();
+          bossHP -= getDamage();
+          console.log(`${namePrompt}'s health is ${userHP}.`);
+          console.log(`Grant's health is ${bossHP}.`);
+        }
+      } else {console.log("You Quit, Game Over!"); break;} 
+    } 
+   } 
+
+// function startCombat() {
+//     let namePrompt = prompt("What is your name?");
+//     if (namePrompt === namePrompt) {
+//             let attackOrQuit = prompt("Would you like to attack or quit?");
+//             if (attackOrQuit === "attack") {
+//             while (attackOrQuit === "attack") {
+//             if (wincondition === 0)  {
+//             console.log(`Grant is donezo, ${namePrompt} is victorious!`)
+//             break;
+//             }
+//             else if (userHP <= 0) {
+//             console.log(`Grant proved to be too mighty, ${namePrompt} has lost the game.`)
+//             break;
+//             }
+//             else if (bossHP <= 0 && userHP != 0 && wincondition != 0) {
+//             bossHP = 10;
+//             wincondition--;
+//             console.log(`With a mighty blow from ${namePrompt}, Grant falls to the ground`);
+//             }  
+//             else {
+//             userHP -= getDamage();
+//             bossHP -= getDamage();
+//             console.log(`${namePrompt}'s health is ${userHP}.`);
+//             console.log(`Grant's health is ${bossHP}.`);
+//             }
+//         } //while loop
+//     } else {console.log("Game Over");}
+//     }//master if
+// } //startcombat function
